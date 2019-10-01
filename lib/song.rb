e0641ceee783ff49eb4443db7b8f4a284e48a13c
Matthew Ryan Palmer <matthewpalmer9@outlook.com>
2019-10-01 20:42:38 +0000

    Automatically backed up by Learn

---
+++ lib/song.rb
@@ -0,0 +1,44 @@
+class Song 
+  attr_accessor :name, :artist, :genre
+  
+  @@count = 0
+  @@genres = []
+  @@artists = []
+  @@genre_count = {}
+  
+  def initialize(name, artist, genre)
+    @name = name 
+    @artist = artist 
+    @genre = genre 
+    @@count += 1
+    
+    @@genres << genre #if !@@genres.include?(genre)
+    
+    @@artists << artist #if !@@artists.include?(artist)
+    
+  end 
+  
+  def self.count
+    @@count
+  end 
+  
+  def self.genres
+    @@genres.uniq
+  end
+  
+  def self.artists
+    @@artists.uniq
+  end
+  
+  def self.genre_count
+    newHash = {}
+    @@genres.each(){|genre|
+      if newHash.include?(genre)
+        newHash[genre] += 1
+      else 
+        newHash[genre] = 1 
+      end
+    }
+    newHash
+  end
+end 
